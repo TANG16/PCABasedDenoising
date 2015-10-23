d2e6450494b524852504860a57b65d07b8ca05a9
Ayush Baid <ayushrakeshbaid@gmail.com>
2015-10-24 00:18:19 +0530

    Code for creating patch based Matrices

---
+++ closestPatchSearch.m
@@ -0,0 +1,7 @@
+function [] = closestPatchSearch(M, K)
+% Performs the nearest neighbour search in a window for the patch based matrix M
+% See function createPatchMatrices for the structure of M
+%   param M: patch based matrix
+%   param K: window size
+
+
